32c8818d7d27f7b47fccbc0f8fa40971e309b353
ankitxcoding <ankitxcoding@gmail.com>
2024-02-15 12:02:25 +0530

    moveznseriez

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="m-4">
+      <h1 className="font-bold text-4xl text-red-600">Testing</h1>
+    </div>
+  );
+}
+
+export default App;
