d7a632f06d63711cecd1a8f99d0a31acc61cd7c1
ankitxcoding <ankitxcoding@gmail.com>
2024-04-07 19:03:07 +0530

    added navigation buttons in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import useStatus from "../hooks/useStatus";
 import logo from "../assets/logo.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
   const networkStatus = useStatus();
+  const location = useLocation();
+  const isHome = location.pathname === "/";
+  const isMoviesPage = location.pathname === "/movies";
+  const isTVShowsPage = location.pathname === "/tvShows";
 
   return (
     <div className="flex justify-between bg-gradient-to-b from-black absolute top-0 w-full">
@@ -14,6 +18,40 @@ const Header = () => {
       </div>
       <div className="flex items-center mx-4">
         <ul className="flex">
+          {!isHome ? (
+            <Link to={"/"}>
+              <li className="m-4 text-white text-xl font-bold hover:cursor-pointer relative w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center">
+                Home
+              </li>
+            </Link>
+          ) : (
+            <>
+              <Link to={"/movies"}>
+                <li className="m-4 text-white text-xl font-bold hover:cursor-pointer relative w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center">
+                  Movies
+                </li>
+              </Link>
+              <Link to={"tvShows"}>
+                <li className="m-4 text-white text-xl font-bold hover:cursor-pointer relative w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center">
+                  Series
+                </li>
+              </Link>
+            </>
+          )}
+          {isMoviesPage && (
+            <Link to={"tvShows"}>
+              <li className="m-4 text-white text-xl font-bold hover:cursor-pointer relative w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center">
+                Series
+              </li>
+            </Link>
+          )}
+          {isTVShowsPage && (
+            <Link to={"/movies"}>
+              <li className="m-4 text-white text-xl font-bold hover:cursor-pointer relative w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center">
+                Movies
+              </li>
+            </Link>
+          )}
           <li className="m-4 text-white text-xl font-bold">
             Status{" "}
             {networkStatus ? (
