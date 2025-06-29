90277e0fedc912b44509da5441917ca5cf7301a6
ankitxcoding <ankitxcoding@gmail.com>
2025-06-29 21:32:56 +0530

    Optimize for smaller screens and now user can stream movies

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -9,6 +9,7 @@ import useMovieTrailer from "../hooks/useMovieTrailer";
 import { useState } from "react";
 import useMovieBackdropImg from "../hooks/useMovieBackdropImg";
 import MovieCastList from "./MovieCastList";
+import OttMoviePlayer from "./OttMoviePlayer";
 
 const MovieDetails = () => {
   const { movieId } = useParams();
@@ -23,7 +24,7 @@ const MovieDetails = () => {
     title,
     backdrop_path,
     genres,
-    homepage,
+    id,
     imdb_id,
     vote_average,
     tagline,
@@ -53,34 +54,40 @@ const MovieDetails = () => {
         className="flex flex-col justify-center items-center min-h-screen"
         style={backgroundStyle}
       >
-        <div className="mt-24 flex flex-col justify-center items-center rounded-md overflow-hidden bg-black bg-opacity-50">
-          <img src={BASE_POSTER_URL + backdrop_path} alt={title} />
-          <h1 className="m-1 text-white text-3xl font-bold">{title}</h1>
-          <h2 className="m-1 text-stone-400 text-sm font-semibold">
+        <div className="mt-16 sm:mt-24 flex flex-col justify-center items-center rounded-md overflow-hidden bg-black bg-opacity-50 w-full sm:w-auto px-4 sm:px-0">
+          <img
+            src={BASE_POSTER_URL + backdrop_path}
+            alt={title}
+            className="w-full sm:w-auto"
+          />
+          <h1 className="m-1 text-white text-xl sm:text-3xl font-bold">
+            {title}
+          </h1>
+          <h2 className="m-1 text-stone-400 text-xs sm:text-sm font-semibold">
             ({tagline === "" ? "NA" : tagline})
           </h2>
-          <p className="m-1 text-white text-xs whitespace-normal max-w-sm text-justify">
+          <p className="m-1 text-white text-xs whitespace-normal max-w-[90%] sm:max-w-sm text-justify">
             {overview}
           </p>
-          <h2 className="m-1 text-stone-400 text-xs">
+          <h2 className="m-1 text-stone-400 text-[10px] sm:text-xs">
             Release Date - {release_date} · Duration - {runtime} Minutes
           </h2>
-          <p className="m-1 text-stone-400 text-xs">
+          <p className="m-1 text-stone-400 text-[10px] sm:text-xs">
             Release Status - ({status})
           </p>
-          <h2 className="m-1 text-white text-sm font-semibold">
+          <h2 className="m-1 text-white text-xs sm:text-sm font-semibold">
             {formattedGenres}
           </h2>
-          <div className="flex mb-2">
+          <div className="flex flex-wrap sm:flex-nowrap justify-center gap-2 mb-2">
             <a
               href={IMDB_URL + imdb_id}
               target="_blank"
               rel="noopener noreferrer"
             >
-              <div className="m-1 px-2 py-1 border border-white flex items-center w-fit bg-black hover:bg-zinc-900 rounded-md">
-                <i className="fa-solid fa-star text-yellow-500 mx-1"></i>
+              <div className="h-9 m-1 px-4 py-1.5 border border-white flex items-center justify-center bg-black hover:bg-zinc-900 rounded-md">
+                <i className="fa-solid fa-star text-yellow-500 text-base"></i>
                 <h2
-                  className={`font-semibold ${
+                  className={`text-base font-semibold ${
                     vote_average >= 7
                       ? "text-green-500"
                       : vote_average >= 6
@@ -94,33 +101,29 @@ const MovieDetails = () => {
                     vote_average && vote_average.toFixed(1)
                   )}
                 </h2>
-                <h3 className="text-gray-400 text-xs mr-1">/10</h3>
+                <h3 className="text-gray-400 text-xs">/10</h3>
               </div>
             </a>
             <button
               onClick={toggleVideo}
-              className="m-1 px-2 py-1 text-white font-semibold bg-[#E50914] hover:bg-[#e50914c0] rounded-md"
+              className="h-9 m-1 px-4 py-1.5 text-white text-base font-semibold bg-[#E50914] hover:bg-[#e50914c0] rounded-md"
             >
               {showTrailer ? "Hide Trailer" : "Watch Trailer"}
             </button>
-            <a href={homepage} target="_blank" rel="noopener noreferrer">
-              <button className="m-1 px-2 py-1 text-black font-semibold bg-white hover:bg-zinc-400 rounded-md">
-                Watch Movie
-              </button>
-            </a>
+            <OttMoviePlayer tmdb_id={id} />
           </div>
         </div>
-        <div className="no-scrollbar my-5 py-2 overflow-x-scroll w-1/2 bg-black bg-opacity-50 rounded-md">
+        <div className="no-scrollbar my-5 py-2 overflow-x-scroll w-[90%] sm:w-1/2 bg-black bg-opacity-50 rounded-md">
           <MovieCastList />
         </div>
         {showTrailer && movieTrailer ? (
-          <div className="video-container absolute">
+          <div className="video-container absolute w-full sm:w-auto">
             <iframe
-              width="1080"
-              height="608"
+              width="100%"
+              height="300"
+              className="sm:w-[1080px] sm:h-[608px]"
               src={`https://www.youtube.com/embed/${movieTrailer}?autoplay=1&mute=1`}
               title="YouTube video player"
-              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               referrerPolicy="strict-origin-when-cross-origin"
               allowFullScreen
@@ -129,18 +132,20 @@ const MovieDetails = () => {
               onClick={toggleVideo}
               className="px-[7px] text-white bg-black hover:bg-[#E50914] rounded-full absolute -top-5 -right-4"
             >
-              <i className="fa-solid fa-xmark text-2xl"></i>
+              <i className="fa-solid fa-xmark text-xl sm:text-2xl"></i>
             </button>
           </div>
         ) : (
           showTrailer && (
-            <div className="w-2/3 h-5/6 flex justify-center items-center bg-black absolute">
-              <p className="text-white text-7xl">Trailer Not Found!</p>
+            <div className="w-[90%] sm:w-2/3 h-5/6 flex justify-center items-center bg-black absolute">
+              <p className="text-white text-3xl sm:text-7xl text-center">
+                Trailer Not Found!
+              </p>
               <button
                 onClick={toggleVideo}
                 className="px-[7px] text-white bg-black hover:bg-[#E50914] rounded-full absolute -top-5 -right-4"
               >
-                <i className="fa-solid fa-xmark text-2xl"></i>
+                <i className="fa-solid fa-xmark text-xl sm:text-2xl"></i>
               </button>
             </div>
           )
@@ -149,4 +154,5 @@ const MovieDetails = () => {
     </>
   );
 };
+
 export default MovieDetails;
