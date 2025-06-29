ac4e2ce3f29cad2f3301159d0e656ede2eb71912
ankitxcoding <ankitxcoding@gmail.com>
2025-06-29 21:28:46 +0530

    Optimize body for the smaller screens

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,17 +5,16 @@ const Body = () => {
   return (
     <>
       <div
-        className="flex items-center justify-center h-screen"
+        className="flex items-center justify-center h-screen bg-center bg-cover"
         style={{
           background: `url(${bgBanner})`,
-          backgroundSize: "cover",
         }}
       >
-        <div className="flex flex-col">
-          <button className="m-5 px-2 text-white bg-transparent text-8xl font-semibold border-2 border-white rounded-lg hover:bg-white hover:text-black duration-300">
+        <div className="flex flex-col px-4">
+          <button className="m-2 sm:m-5 px-2 text-white bg-transparent text-6xl sm:text-7xl md:text-8xl lg:text-9xl font-semibold border-2 border-white rounded-lg hover:bg-white hover:text-black duration-300">
             <Link to={"/movies"}>MOVIES</Link>
           </button>
-          <button className="m-5 px-2 text-white bg-transparent text-8xl font-semibold border-2 border-white rounded-lg hover:bg-white hover:text-black duration-300">
+          <button className="m-2 sm:m-5 px-2 text-white bg-transparent text-6xl sm:text-7xl md:text-8xl lg:text-9xl font-semibold border-2 border-white rounded-lg hover:bg-white hover:text-black duration-300">
             <Link to={"/tvShows"}>SERIES</Link>
           </button>
         </div>
