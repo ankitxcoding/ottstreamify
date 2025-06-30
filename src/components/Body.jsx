284e32c4bda46aa765ee4401404f44a4d1e4da26
ankitxcoding <ankitxcoding@gmail.com>
2025-06-30 19:42:22 +0530

    changed homepage text from series to tv shows and font sizes

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -14,11 +14,11 @@ const Body = () => {
         }}
       >
         <div className="flex flex-col px-4">
-          <button className="m-2 sm:m-5 p-2 text-white bg-transparent text-6xl sm:text-7xl md:text-8xl lg:text-9xl font-semibold border-2 border-white rounded-lg hover:bg-white hover:text-black duration-300">
+          <button className="m-2 sm:m-5 p-2 text-white bg-transparent text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-semibold border-2 border-white rounded-lg hover:bg-white hover:text-black duration-300">
             <Link to={"/movies"}>MOVIES</Link>
           </button>
-          <button className="m-2 sm:m-5 p-2 text-white bg-transparent text-6xl sm:text-7xl md:text-8xl lg:text-9xl font-semibold border-2 border-white rounded-lg hover:bg-white hover:text-black duration-300">
-            <Link to={"/tvShows"}>SERIES</Link>
+          <button className="m-2 sm:m-5 p-2 text-white bg-transparent text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-semibold border-2 border-white rounded-lg hover:bg-white hover:text-black duration-300">
+            <Link to={"/tvShows"}>TV SHOWS</Link>
           </button>
         </div>
       </div>
